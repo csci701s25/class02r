4d8c81aa3578ae0a8c04a2e2645a73ffb40c3429
Lukka Wolff <lawolff@middlebury.edu>
2025-02-20 15:31:02 +0000

    Changed lets to consts and changed getColor4 to the best color on earth ever created in the history of anything

--- app.js
+++ app.js
@@ -3,27 +3,27 @@ const add = function (x, y) {
 };
 
 const getColor1 = function () {
-  let color = [0, 0, 0];
+  const color = [0, 0, 0];
   return color;
 };
 
 const getColor2 = function () {
-  let color = [32, 50, 70];
+  const color = [32, 50, 70];
   return color;
 };
 
 const getColor3 = function () {
-  let color = [0, 255, 0];
+  const color = [0, 255, 0];
   return color;
 };
 
 const getColor4 = function () {
-  let color = [0, 0, 255];
+  const color = [95, 44, 122];
   return color;
 };
 
 const getColor5 = function () {
-  let color = [256, 0, 256];
+  const color = [256, 0, 256];
   return color;
 };
 
